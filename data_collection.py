eb95753d8bc9879e6c1f98d5db4ae3d801aa2c09
vittoboa <boarinivittorio@gmail.com>
2019-05-01 23:39:45 +0200

    Move all constants to a separate file

--- data_collection.py
+++ data_collection.py
@@ -1,15 +1,16 @@
 import Team
+import const
 
 import pandas as pd
 from nba_api.stats.endpoints import boxscoresummaryv2
 
 
-TEAMS_DATA = []
-MATCHES_DATA = pd.DataFrame()
+teams_data = []
+matches_data = pd.DataFrame()
 
 
 def save_to_file(file_name):
-    MATCHES_DATA.to_csv(file_name, index=False)
+    matches_data.to_csv(file_name, index=False)
 
 def add_digits(num, digits):
     num_digits = sum(1 for _ in str(num))
@@ -19,12 +20,10 @@ def calculate_game_id(year, game_number):
     return "002" + add_digits(year - 1, 2) + add_digits(game_number, 5)
 
 def get_games_id():
-    NUM_GAMES = 1230
-    first_year, last_year = 19, 19
-    years = (year for year in range(first_year, last_year + 1))
+    years = (year for year in range(const.FIRST_YEAR, const.LAST_YEAR + 1))
     
     for year in years:
-        for game_number in range(1, NUM_GAMES + 1):
+        for game_number in range(1, const.NUM_GAMES + 1):
             yield calculate_game_id(year, game_number)
 
 def get_game(game_id):
@@ -33,40 +32,37 @@ def get_game(game_id):
     return boxscoresummary
 
 def get_game_summary(game):
-    index_game_summary = 0
-    game_summary = game[index_game_summary]["rowSet"][0]
+    game_summary = game[const.INDEX_GAME_SUMMARY]["rowSet"][0]
     return game_summary
 
 def get_line_score(game):
-    index_line_score = 5
-    line_score = game[index_line_score]["rowSet"]
+    line_score = game[const.INDEX_LINE_SCORE]["rowSet"]
     return line_score
 
 def get_team_id(game):
-    index_team_id = 3
     home_line_score, away_line_score = get_line_score(game)
-    home_team_id, away_team_id = home_line_score[index_team_id], away_line_score[index_team_id]
+    home_team_id = home_line_score[const.INDEX_TEAM_ID]
+    away_team_id = away_line_score[const.INDEX_TEAM_ID]
     return home_team_id, away_team_id
 
 def get_season(game):
-    index_season = 8
-    season = get_game_summary(game)[index_season]
+    season = get_game_summary(game)[const.INDEX_SEASON]
     return season
 
 def get_points(game):
-    index_points = 22
     home_line_score, away_line_score = get_line_score(game)
-    home_points, away_points = home_line_score[index_points], away_line_score[index_points]
+    home_points = home_line_score[const.INDEX_POINTS]
+    away_points = away_line_score[const.INDEX_POINTS]
     return home_points, away_points
 
 def add_team(id, season):
-    TEAMS_DATA.append(Team.Team(id, season))
+    teams_data.append(Team.Team(id, season))
 
 def get_team(id, season):
-    return next((team for team in TEAMS_DATA if team.id == id), add_team(id, season))
+    return next((team for team in teams_data if team.id == id), add_team(id, season))
 
 def update_match(season, home_id, away_id, winner):
-    global MATCHES_DATA
+    global matches_data
     home, away = get_team(home_id, season), get_team(away_id, season)
     home_data = [home.get_win_percentage(), home.get_last_5_wins(), home.get_avg_points(), home.get_last_5_points()]
     away_data = [away.get_win_percentage(), away.get_last_5_wins(), away.get_avg_points(), away.get_last_5_points()]
@@ -76,7 +72,7 @@ def update_match(season, home_id, away_id, winner):
                 "winner"]
 
     new_match = pd.DataFrame([home_and_away + [winner]], columns=parameters)
-    MATCHES_DATA = MATCHES_DATA.append(new_match, ignore_index=True)
+    matches_data = matches_data.append(new_match, ignore_index=True)
 
 def update_team(season, team_id, points, opponents_points):
     team = get_team(team_id, season)

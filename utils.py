28c1d01eeb7ed93a8baf60b790db456e70d51b48
vittoboa <boarinivittorio@gmail.com>
2019-09-27 19:02:28 +0200

    Add type annotations

--- utils.py
+++ utils.py
@@ -1,60 +1,63 @@
 import const
+from typing import Tuple, Dict
+
+import pandas as pd
 
 
-def calculate_shooting(fgm, fg3m, fga):
+def calculate_shooting(fgm: float, fg3m: float, fga: float) -> float:
     """ effective field goal """
     return (fgm + fg3m/2) / fga
 
 
-def calculate_poss(fga, oreb, to, fta):
+def calculate_poss(fga: float, oreb: float, to: float, fta: float) -> float:
     """ offensive possession """
     return fga - oreb + to + (0.4 * fta)
 
 
-def calculate_oreb(oreb, opponent_dreb):
+def calculate_oreb(oreb: float, opponent_dreb: float) -> float:
     """ offensive rebounding """
     return oreb / (oreb + opponent_dreb)
 
 
-def calculate_free_thorows(ftm, fga):
+def calculate_free_thorows(ftm: float, fga: float) -> float:
     """ getting to the foul line """
     return ftm / fga
 
 
-def get_four_factors(four_factors_data, opnt_dreb):
-    ffd = four_factors_data
+def get_four_factors(four_factors: Dict[str, float], opnt_dreb: float) -> Dict[str, float]:
+    ff = four_factors
 
-    shooting = calculate_shooting(ffd["fgm"], ffd["fg3m"], ffd["fga"])
-    poss = calculate_poss(ffd["fga"], ffd["oreb"], ffd["to"], ffd["fta"])
-    oreb = calculate_oreb(ffd["oreb"], opnt_dreb)
-    free_throws = calculate_free_thorows(ffd["ftm"], ffd["fga"])
+    shooting = calculate_shooting(ff["fgm"], ff["fg3m"], ff["fga"])
+    poss = calculate_poss(ff["fga"], ff["oreb"], ff["to"], ff["fta"])
+    oreb = calculate_oreb(ff["oreb"], opnt_dreb)
+    free_throws = calculate_free_thorows(ff["ftm"], ff["fga"])
     four_factors = (shooting, poss, oreb, free_throws)
 
     return dict(zip(const.FOUR_FACTORS, four_factors))
 
 
-def get_avg(sum, n):
+def get_avg(sum: int, n: int) -> float:
     return round(sum / n, 3)
 
 
-def save_dataframe_in_file(dataframe, file_name):
+def save_dataframe_in_file(dataframe: pd.DataFrame, file_name: str) -> None:
     dataframe.to_csv(file_name, index=False)
 
 
-def add_digits(num, digits):
+def add_digits(num: int, digits: int) -> str:
     num_digits = sum(1 for _ in str(num))
     return ''.join('0' for _ in range(digits - num_digits)) + str(num)
 
 
-def get_quartiles(dataframe):
-    Q1 = dataframe.quantile(0.25)
-    Q2 = dataframe.quantile(0.50)
-    Q3 = dataframe.quantile(0.75)
+def get_quartiles(df: pd.DataFrame) -> Tuple[pd.Series, pd.Series, pd.Series]:
+    Q1 = df.quantile(0.25)
+    Q2 = df.quantile(0.50)
+    Q3 = df.quantile(0.75)
 
     return Q1, Q2, Q3
 
 
-def remove_outliers(df, target):
+def remove_outliers(df: pd.DataFrame, target: str) -> pd.DataFrame:
     features = df.drop([target], 1)
 
     Q1, _, Q3 = get_quartiles(features)
